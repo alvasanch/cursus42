a9d93ca5978f33e0e235f66a657e576a6a8c4aa3
alvasanch <146002949+alvasanch@users.noreply.github.com>
2023-10-27 09:10:24 +0200

    Add files via upload

---
+++ GNL/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvasanc <alvasanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 10:00:01 by alvasanc          #+#    #+#             */
+/*   Updated: 2023/10/26 12:06:47 by alvasanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+#endif
